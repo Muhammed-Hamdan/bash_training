65a0b0926cbe401fc50c79b8e87f3633d3111194
Muhammed-Hamdan <ahmedhamdan672@gmail.com>
2022-11-25 16:10:25 +0530

    Added files

--- ncert_collect/scrap/scrap1.sh
+++ ncert_collect/scrap/scrap1.sh
@@ -23,17 +23,47 @@ check_field_equals() {
 	fi
 }
 
+# $1 - String of question
+# echo - String <Class-Chapter-Exercise-Qno>
+# return 0 -> valid question, 1 -> invalid question
+extract_question_info() {
+
+	question=$(echo $1 | sed 's/ //g'| grep -iEo 'Q\([0-9]+\),.*Exercise.*,Class[0-9]+')
+	
+	if [ $? -ne 0 ]; then
+		return 1
+	fi
+	
+	class=${question##*[cC]lass}
+
+	chapter=$(echo $question | cut -d, -f2)
+	echo $question | grep -i "Misc" &> /dev/null
+	if [ $? -eq 0 ]; then
+		exercise=misc
+		chapter=${chapter##*[cC]hapter}
+	else
+		exercise=$(echo $chapter| cut -d. -f2)
+		chapter=$(echo $chapter| cut -d. -f1)
+		chapter=${chapter##*[eE]xercise}
+	fi
+
+	qno=${question##*(}
+	qno=${qno%%)*}
+
+	echo "$class-$chapter-$exercise-$qno"
+	return 0
+}
+
 process_record() {
 	id=$(echo $1 | cut -d, -f$idcol)
-	#id=FWC22025
+	qrecord="$(grep $id questions.csv)"
+
 	mkdir $id
 	cd $id
 
 
 	# Preprocessing on links
 	link=$(echo $1 | cut -d, -f$linkcol)
-	#link="https://github.com/madind5668/FWC/tree/main/FWC - 1"
-	#link="https://github.com/ManojChavva/FWC"
 	link=$(echo $link | sed "s/ /%20/g" )
 	link=${link%%.git*}
 	if [ $(echo $link | grep "/tree/main" | wc -l) -ne 0 ]
@@ -42,25 +72,29 @@ process_record() {
 	else
 		link_suffix="/tree/main"
 	fi
+	
 
 	all_complete=1
 	topdirname=""
 	for counter in ${!lowdir[@]}
 	do
-		if $(check_field_equals "$1" $(( $startcol + $counter )) $completed_entry)
-		then
+		if $(check_field_equals "$1" $(( $startcol_mark + $counter )) $completed_entry); then
+
+			qdata=$(extract_question_info "$(echo $qrecord | csvcut -c"$(( $startcol_question + $counter ))")")
+			[ $? -ne 0 ] && continue
+
 			if [ $(echo $topdirname | grep -i ${topdir[$counter]} | wc -l) -eq 0 ]
 			then
 				topdirname=$(extract_dirname_githubpage $link ${topdir[$counter]} | sed "s/ /%20/g")
 			fi
 			lowdirname=$(extract_dirname_githubpage $link$link_suffix/$topdirname ${lowdir[$counter]} | sed "s/ /%20/g")
 			echo $topdirname : $lowdirname
-			#svn co $link/trunk/$topdirname/$lowdirname ${lowdir[$counter]} &> /dev/null
+			svn co $link/trunk/$topdirname/$lowdirname ${lowdir[$counter]} &> /dev/null
+
 		else
 			echo $id completed upto ${topdir[$(( $counter - 1 ))]}:${lowdir[$(( $counter - 1 ))]}
 			all_complete=0
 			break
-		fi
 	done
 	
 	if [ $all_complete -eq 1 ]
@@ -72,10 +106,12 @@ process_record() {
 	rm -rf $id
 }
 
+
 # Global variables
 linkcol=4
 idcol=2
-startcol=14
+startcol_mark=14
+startcol_question=9
 topdir=("matrices" "matrices" "matrices" "optimization" "optimization")
 lowdir=("line" "circle" "conic" "basic" "advanced")
 completed_entry=5
@@ -85,7 +121,6 @@ completed_entry=5
 #
 process_record "$(grep Hamdan marks.csv)"
 #process_record "$(grep -i chavva marks.csv)"
-#process_record "$(grep FWC22025 marks.csv)"
 
 # Quote variables to preserve newline
 #while read -r item
